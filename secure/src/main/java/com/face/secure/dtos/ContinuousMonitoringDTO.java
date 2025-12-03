a84c7875670a7d2019a29943f3e1f502f6752a38
savioemanuelf <savioemanuel2@gmail.com>
2025-12-03 13:35:05 -0300

    fix: resolve JML errors in ContinuousMonitoringDTO

--- secure/src/main/java/com/face/secure/dtos/ContinuousMonitoringDTO.java
+++ secure/src/main/java/com/face/secure/dtos/ContinuousMonitoringDTO.java
@@ -1,30 +1,61 @@
 package com.face.secure.dtos;
 
 public class ContinuousMonitoringDTO {
-    private int minutes;
-    private int timeLimit;
+    
+    private /*@ spec_public @*/ int minutes;
+    private /*@ spec_public @*/ int timeLimit;
 
-    public int getMinutes() {
-        return minutes;
+    //@ public invariant minutes >= 0;
+    //@ public invariant timeLimit >= 0;
+
+    public ContinuousMonitoringDTO() {
     }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
-
-    public int getTimeLimit() {
-        return timeLimit;
-    }
-
-    public void setTimeLimit(int timeLimit) {
-        this.timeLimit = timeLimit;
-    }
-
+    /*@
+      @ public normal_behavior
+      @   requires minutes >= 0;
+      @   requires timeLimit >= 0;
+      @   ensures this.minutes == minutes;
+      @   ensures this.timeLimit == timeLimit;
+      @*/
     public ContinuousMonitoringDTO(int minutes, int timeLimit) {
         this.minutes = minutes;
         this.timeLimit = timeLimit;
     }
 
-    public ContinuousMonitoringDTO() {
+    /*@
+      @ public normal_behavior
+      @   ensures \result == this.minutes;
+      @*/
+    public /*@ pure @*/ int getMinutes() {
+        return minutes;
+    }
+
+    /*@
+      @ public normal_behavior
+      @   requires minutes >= 0;
+      @   assignable this.minutes;
+      @   ensures this.minutes == minutes;
+      @*/
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    /*@
+      @ public normal_behavior
+      @   ensures \result == this.timeLimit;
+      @*/
+    public /*@ pure @*/ int getTimeLimit() {
+        return timeLimit;
+    }
+
+    /*@
+      @ public normal_behavior
+      @   requires timeLimit >= 0;
+      @   assignable this.timeLimit;
+      @   ensures this.timeLimit == timeLimit;
+      @*/
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
     }
 }
